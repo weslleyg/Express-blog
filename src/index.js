33a60ddc938b46932b8f0dde754dd7be65a038b0
Weslley Guilherme <wesley.dipaulatorres@hotmail.com>
2019-06-25 14:38:26 -0300

    organização das pastas

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ mongoose.connect(config.connectionString, {
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'front', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'front', 'public')));
 app.use(logger('dev'));
 app.use(cors());
 app.use(
@@ -48,18 +48,18 @@ app.use('/post', postRoute);
 
 app.use(function(req, res, next) {
 	next(createError(404));
-  });
-  
-  // error handler
-  app.use(function(err, req, res, next) {
+});
+
+// error handler
+app.use(function(err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
 	// render the error page
 	res.status(err.status || 500);
 	res.render('error');
-  });
+});
 
 module.exports = {
 	app,
